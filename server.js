c6fe6fc4522105b7600bd86ae918c63d7f5cbfe6
SirBadr <tso1999@yahoo.com>
2023-08-14 00:18:49 +0300

    updates

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 
 const port = 1605; 
 
-app.get('/', (req, res) => {
-  res.send('Hi bashmohandes mahmoud');
+app.get('/', (req, res) => {   // return should be in JSON format.
+  return ({
+    success: true,
+    message:"Hi Asmaa :D",
+    data: {
+      
+    }
+  });
 });
 
 app.listen(port);
